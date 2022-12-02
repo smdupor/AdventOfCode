1f07794446f6670308d2025ec76ca7e631cd8d05
Stevan Dupor <smdupor@gmail.com>
2022-12-02 00:13:20 -0500

    Solution to AOC Day 2

--- Day02/aoc.py
+++ Day02/aoc.py
@@ -1,18 +1,40 @@
 fp = open("input.txt","r")
 result = []
 i = int(0)
-
+j = int(0)
+X=1
+Y=2
+Z=3
+lose = 0
+draw = 3
+win = 6
 
 for l in fp:
- #   print(l)
-    if(len(l) > 1):
-        i += int(l)
-    else:
-        #print(result)
-        result.append(i)
-        i = int(0)
+    me = 0
+    if(l[2] == 'X'):
+        me += lose
+        if(l[0] == 'C'):
+            me += Y
+        elif(l[0] == 'A'):
+            me += Z
+        else:
+            me += X
+    elif(l[2] == 'Y'):
+        me += draw
+        if(l[0] == 'A'):
+            me += X
+        elif(l[0] == 'B'):
+            me += Y
+        else:
+            me += Z
+    elif(l[2] == 'Z'):
+        me += win
+        if(l[0] == 'B'):
+            me += Z
+        elif(l[0] == 'C'):
+            me += X
+        else:
+            me += Y
+    i += me
 
-result.sort()
-
-print(result[-1])
-print(result[-1] + result[-2] + result[-3])
+print(str(i))
