cbaab28928144f4a17de498dac2acfb58383e79a
Stevan Dupor <smdupor@gmail.com>
2022-12-07 07:36:54 -0500

    Day 7 initial soln

--- Day07/aoc.py
+++ Day07/aoc.py
@@ -7,64 +7,153 @@ from itertools import *
 def part1():
     fp = open("input.txt","r")
     text = fp.read()
-    
-    for i in range(0,len(text)):
-        uniq = []
-        for j in range(i, i+4):
-            if text[j] not in uniq:
-                uniq.append(text[j])
+    data = text.split("\n")
 
-        if len(uniq) == 4:
-            prt_red(i+4)
-            return
+    dirs = deque()
+
+    sizes = dict()
+
+    curr = ""
+    data = data[:-1]
+    n=0
+    for d in data:
+        n+=1
+        if d[0:7] == "$ cd ..":
+            print(d)
+            dirs.pop()
+            currsz = sizes[curr]
+            curr = ""
+            for d in dirs:
+                curr = curr + d 
+                sizes[curr] += currsz
+        elif d[0:5] == "$ cd ":
+            print(d)
+            dirs.append(d[5:])
+            print(dirs)
+            curr = ""
+            for d in dirs:
+                curr = curr + d 
+            sizes[curr] = 0
+            
+
+        elif d[0:4] == "dir " or d[0:4]=="$ ls":
+            t=0
+        else:
+            print(d)
+            sizes[curr] += int(d.split(" ")[0])
+
+    n=0
+    m=0
+    for s in sizes:
+        if sizes[s] < 100000:
+            n += 1
+            m += sizes[s]
+
+    prt_red(n)
+    prt_red(m)
+
+
 
 def part1_naive():
     fp = open("input.txt","r")
     text = fp.read()
-    pkt = deque()
-    ind = deque()
 
-    for i in range(0,len(text)):
-        
-        # Exit condition
-        if len(pkt) == 4:
-            prt_red(i-2)
-            return
-        
-        # Slide the window forward if not found
-        # By popping only if a unique character is exiting the window
-        if len(pkt) >0 and ind[0] == i-4:
-            ind.popleft()
-            pkt.popleft()
-        
-        #Then pushing if a unique character is about to enter
-        if text[i] not in pkt:
-            pkt.append(text[i])
-            ind.append(i)
-        
+    return
+
+    fp.close()  
 ############################################################ PART 2 ############################################
 
 def part2():
     fp = open("input.txt","r")
     text = fp.read()
+    data = text.split("\n")
 
-    for i in range(0,len(text)):
-        uniq = []
-        for j in range(i, i+14):
-            if text[j] not in uniq:
-                uniq.append(text[j])
+    dirs = deque()
 
-        if len(uniq) == 14:
-            prt_red(i+14)
-            return
+    sizes = dict()
 
-    fp.close()
+    sizes_uniq = dict()
+    curr = ""
+    data = data[:-1]
+    n=0
+    prevdd = False
+    for d in data:
+        n+=1
+        if d[0:7] == "$ cd ..":
+            #print(d)
+            dirs.pop()
+            #if not prevdd:
+            currsz = sizes[curr]
+            sizes_uniq[curr] = sizes[curr]
+             #   prevdd = True
+                #for d in dirs:
+                    #sizes[curr] += currsz
+            
+            curr = ""
+            for d in dirs:
+                curr = curr + d 
+            sizes[curr] += currsz
+                
+        elif d[0:5] == "$ cd ":
+            prevdd = False
+            #print(d)
+            dirs.append(d[5:])
+            #print(dirs)
+            curr = ""
+            for d in dirs:
+                curr = curr + d 
+            sizes[curr] = 0
+            
+
+        elif d[0:4] == "dir " or d[0:4]=="$ ls":
+            t=0
+        else:
+            #print(d)
+            sizes[curr] += int(d.split(" ")[0])
+    
+
+
+    while len(dirs) > 1:
+        dirs.pop()
+        
+        currsz = sizes[curr]
+        sizes_uniq[curr] = sizes[curr]
+    
+    
+        curr = ""
+        for d in dirs:
+            curr = curr + d 
+    
+        sizes[curr] += currsz
+
+    
+    n=0
+    m=2**32
+
+    print(70000000 - sizes["/"])
+
+    cnst = 30000000 - (70000000 - sizes["/"])
+            
+    prt_grn(cnst)
+
+    for s in sizes:
+        
+        if sizes[s] > cnst:
+            print(sizes[s])
+            if sizes[s]<m:
+                m = sizes[s]
+                
+
+    prt_red(n)
+    prt_red(m)
+
+    #print(sizes)
 
 def part2_naive():
     fp = open("input.txt","r")
     text = fp.read()
-    data = text.split("\n")
-    stack = deque()
+
+    return
 
     fp.close()
 
