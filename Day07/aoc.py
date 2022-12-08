83ab22b8e29135ab211e971ac1d546aeffcbb86d
Stevan Dupor <smdupor@gmail.com>
2022-12-08 02:09:16 -0500

    More day7 cleanup

--- Day07/aoc.py
+++ Day07/aoc.py
@@ -11,76 +11,76 @@ def part1():
 def part1_naive():
     fp = open("input.txt","r")
     text = fp.read()
-    data = text.split("\n")
-    data = data[:-1]
+    fp.close()
+    data = text.split("\n")[:-1]
     
     dirs = deque()
     sizes = dict()
-    curr = ""
+    curr_path = ""
     
-    n=0
+    # Use an implied state machine, where state transitions are triggered by cd commands.
     for d in data:
-        n+=1
 
-        # State: Exiting a directory upward. Commit the counted bytes to this directory and its direct parent only.
-        if d[0:7] == "$ cd ..":
-            dirs.pop()
-            currsz = sizes[curr]
-            curr = ""
-            for d in dirs:
-                curr = curr + d 
-            sizes[curr] += currsz
-        
-        # State: Entering a subdirectory. Initialize size and full-path name of the subdirectory
-        elif d[0:5] == "$ cd ":
+        # STATE: Entering a subdirectory. 
+        #        Initialize size and full-path name of the subdirectory
+        if d[0:5] == "$ cd ":
             dirs.append(d[5:])
-            curr = ""
-            for d in dirs:
-                curr = curr + d 
-            sizes[curr] = 0
-        
-        # State: Swallow "ls" commands and "dir" listings
-        elif d[0:4] == "dir " or d[0:4]=="$ ls":
-            t=0
-        
-        # State: Found a file. Capture number of bytes into current working directory total.
-        else:
-            sizes[curr] += int(d.split(" ")[0])
+            curr_path = get_full_path(dirs)
+            sizes[curr_path] = 0
 
-    # Because we can stop without committing upwards all the way to root, for the last branch searched
-    # Pop all directories up to root, committing the last set of byte counts
+        # STATE: Exiting a directory upward. 
+        #        Commit the counted bytes to this directory and its direct parent only.
+        elif d[0:7] == "$ cd ..":
+            dirs.pop()
+            curr_size = sizes[curr_path]
+            curr_path = get_full_path(dirs)
+            sizes[curr_path] += curr_size
+        
+        # STATE: Swallow "ls" commands and "dir" listings
+        elif d[0:4] == "dir " or d[0:4]=="$ ls":
+            pass
+        
+        # STATE: Found a file. 
+        #        Capture number of bytes into current working directory total.
+        else:
+            sizes[curr_path] += int(d.split(" ")[0])
+
+    # Because the program trace ends in the middle of a branch or on a leaf, so there are 
+    # uncommitted sums, we need to do the tail-end of committing the summations up to root.
     while len(dirs) > 1:
         dirs.pop()
-        currsz = sizes[curr]
-        curr = ""
-        for d in dirs:
-            curr = curr + d 
-
-        sizes[curr] += currsz
+        curr_size = sizes[curr_path]
+        curr_path = get_full_path(dirs)
+        sizes[curr_path] += curr_size
 
     # Part 1: Compile sum of sizes of dirs < 100k bytes
     n=0
-    m=0
+    min_file_size=0
     for s in sizes:
-        if sizes[s] < 100000:
+        if sizes[s] < 1e5:
             n += 1
-            m += sizes[s]
+            min_file_size += sizes[s]
 
-    prt_red(m)
+    prt_red(min_file_size)
 
     # Part 2: Figure out  the smallest directory to free, to have 3mm Bytes, or more, available
     prt_grn("Part 2:")
-    n=0
-    m=2**32
 
-    cnst = 30000000 - (70000000 - sizes["/"])
+    min_file_size = 2**32
+    bytes_requested = 3e7 - (7e7 - sizes["/"])
 
     for s in sizes:
-        if sizes[s] > cnst and sizes[s]<m:
-                m = sizes[s]
+        if sizes[s] > bytes_requested and sizes[s] < min_file_size:
+                min_file_size = sizes[s]
 
-    prt_red(m)
-    fp.close()
+    prt_red(min_file_size)
+    
+
+def get_full_path(dirs):
+    path = ""
+    for d in dirs:
+        path = path + d 
+    return path
 
 ############################################################ PART 2 ############################################
 
