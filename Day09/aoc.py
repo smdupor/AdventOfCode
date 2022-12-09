754d335681865836e5d6a3d9eb95c9edb368e77f
Stevan Dupor <smdupor@gmail.com>
2022-12-09 01:52:10 -0500

    Day 9 Solution Complete

--- Day09/aoc.py
+++ Day09/aoc.py
@@ -1,66 +1,128 @@
-from math import floor, ceil
 from aocutils import *
-from collections import deque
-from itertools import *
 
-############################################################ PART 1 ############################################
-def part1():
-    fp = open("input.txt","r")
-    text = fp.read()
-    data = text.split("\n")
+# Define constants
+NECK = ROW = NECKPTR = 0
+TAIL = COL = 1
+TAILPTR = 8
 
-    
-    fp.close()
+def is_direct_adjacent(h,t):
+    if h == t:
+        return True
+    if (h[ROW] == t[ROW] and (t[COL] == h[COL] - 1 or t[COL] == h[COL]+1)) or (h[COL] == t[COL] and (t[ROW] == h[ROW]-1 or t[ROW] == h[ROW] + 1)):
+        return True
+    return False
 
-def part1_naive():
+def is_diagonal_adjacent(h,t):
+    if (t[ROW] == h[ROW] - 1 or t[ROW] == h[ROW] + 1) and (t[COL] == h[COL]-1 or t[COL] == h[COL] + 1):
+        return True
+    return False
+
+def move_hptr(dir, hptr):
+    if dir == "U":
+        hptr[ROW] += 1
+    elif dir == "D":
+        hptr[ROW] -= 1
+    elif dir == "L":
+        hptr[COL] -= 1
+    elif dir == "R":
+        hptr[COL] += 1
+    return hptr
+
+def move_tptr(h,t):
+    if not (is_direct_adjacent(h,t) or is_diagonal_adjacent(h,t)):
+        diff = [0,0]
+        diff[ROW] = h[ROW] - t[ROW]
+        diff[COL] = h[COL] - t[COL]
+
+        # Left or right move
+        if diff[ROW] == 0:
+            t[COL] = t[COL] - 1 if diff[COL] < 0 else t[COL] + 1 
+        # Up or Down Move
+        elif diff[COL] == 0:
+            t[ROW] = t[ROW] - 1  if diff[ROW] < 0 else  t[ROW] + 1 
+        # Diagonal Dn/Rt
+        elif diff[ROW] > 0 and diff[COL] > 0:
+            t[ROW] += 1
+            t[COL] += 1
+        # Diagonal Up/Rt
+        elif diff[ROW] < 0 and diff[COL] > 0:
+            t[ROW] -= 1
+            t[COL] += 1
+        # Diagonal Dn/Lft
+        elif diff[ROW] > 0 and diff[COL] < 0:
+            t[ROW] += 1
+            t[COL] -= 1
+        # Diagonal Up/Lft
+        elif diff[ROW] < 0 and diff[COL] < 0:
+            t[0] -= 1
+            t[COL] -= 1
+    return t
+
+def aoc_day_09():
     fp = open("input.txt","r")
     text = fp.read()
     data = text.split("\n")
     data = data[:-1]
+    
+    #Init Breadcrumb matrices
+    tail_breadcrumbs = [[],[]]
+    for j in range(0,1000):
+        tail_breadcrumbs[NECK].append([])
+        tail_breadcrumbs[TAIL].append([])
+        for i in range(0,1000):
+            tail_breadcrumbs[NECK][j].append(0)
+            tail_breadcrumbs[TAIL][j].append(0)
+    
+    # Init head/neck/tail pointers
+    hptr = [500,500]
+    tptr_list = []
+    for n in range(0,9):
+        tptr_list.append([500,500])
 
-    # Set up both a matrix of the treemap, and a visibility matrix (Initialize as all-invisible)
-    # mtx = []
-    # viz = []
-    # i=-1
-    # for d in data:
-    #     mtx.append([])
-    #     viz.append([])
-    #     i += 1
-    #     for e in d:
-    #         mtx[i].append(int(e))
-    #         viz[i].append(0)
+    # And mark their location in the breadcrumb map
+    tail_breadcrumbs[NECK][tptr_list[NECKPTR][ROW]][tptr_list[NECKPTR][COL]] = 1
+    tail_breadcrumbs[TAIL][tptr_list[TAILPTR][ROW]][tptr_list[TAILPTR][COL]] = 1
+    
+    # Traverse the flatfile
+    for d in data:
+        directions = d.split(" ")
+        distance = int(directions[1])
 
-    prt_red(0)
+        while(distance > 0):
+            #  Move the head pointer
+            hptr = move_hptr(directions[0],hptr)
+            distance -=1
 
-    # Part 2
+            # Move the first tail pointer
+            tptr_list[NECK] = move_tptr(hptr,tptr_list[NECKPTR])
 
-    prt_grn("Part 2:")
-    prt_red(0)
+            # Move the rest of the tail pointers
+            for n in range(1,9):
+                tptr_list[n] = move_tptr(tptr_list[n-1], tptr_list[n])
+            
+            # Update the breadcrumbs map
+            tail_breadcrumbs[NECK][tptr_list[NECKPTR][ROW]][tptr_list[NECKPTR][COL]] = 1
+            tail_breadcrumbs[TAIL][tptr_list[TAILPTR][ROW]][tptr_list[TAILPTR][COL]] = 1
+    
+    prt_grn("PART 1:")
+    
+    visited_qty = 0
+    for tptr_list in tail_breadcrumbs[NECK]:
+        for t in tptr_list:
+            visited_qty += t
+    
+    prt_red(visited_qty)
+
+
+    prt_grn("PART 2:")
+
+    visited_qty = 0
+    for tptr_list in tail_breadcrumbs[TAIL]:
+        for t in tptr_list:
+            visited_qty += t
+    
+    prt_red(visited_qty)
 
     fp.close()  
 
-############################################################ PART 2 ############################################
-
-def part2():
-    fp = open("input.txt","r")
-    text = fp.read()
-    data = text.split("\n")
-    fp.close()
-
-    #print(sizes)
-
-def part2_naive():
-    fp = open("input.txt","r")
-    text = fp.read()
-
-    return
-
-    fp.close()
-
-prt_grn("\nPart 1:")
-part1()
-part1_naive()
-# prt_grn("\nPart 2:")
-# part2()
-# part2_naive()
-# print("")
+aoc_day_09()
