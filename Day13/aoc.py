c6e99d093640727b3e587328e15aa7b2c5a02d15
Stevan Dupor <smdupor@gmail.com>
2022-12-13 23:22:49 -0500

    Implement parsing of flatfile into packed lists

--- Day13/aoc.py
+++ Day13/aoc.py
@@ -1,54 +1,18 @@
 from aocutils import *
-
+from printed_parsing import *
 def aoc_day11():
-    fp = open("test_input.txt","r")
+    fp = open("input.txt","r")
     text = fp.read()
     fp.close() 
-    
+    data=text[:-1].split("\n")
 
-class Monkey:
-    items = []
-    def __init__(self, items, operation, boolean, t, f, div = 1):
-        self.items = items
-        self.op = operation
-        self.test = boolean
-        self.t = t
-        self.f = f
-        self.inspections = 0
-        self.divisor = div
-
-    def process(self, monkeys):
-        for item in self.items:
-            self.inspections += 1
-            temp = self.op(item)
-            temp //= 3
-            if self.test(temp):
-                monkeys[self.t].items.append(temp)
-            else:
-                monkeys[self.f].items.append(temp)
-        self.items = []
-
-    def process_p2(self, monkeys):
-        for item in self.items:
-            self.inspections += 1
-            temp = self.op(item)
-            temp %= 9699690
-            if self.test(temp):
-                monkeys[self.t].items.append(temp)
-            else:
-                monkeys[self.f].items.append(temp)
-        self.items = []
-                
-
-    def __str__(self):
-        val = "(" + str(self.inspections) +") Mnk: "
-        for i in self.items:
-            val += str(i) + ", "
-        
-        return val
+    data = load_lists(data)
+    #test_loaded_data(data)
 
 
     
+    
 
-prt_grn("\nPart 1:")
+
+# prt_grn("\nPart 1:")
 aoc_day11()
