cf39284e8a6d5111f2793ae5ed22355d2638c1c9
Stevan Dupor <stevan.dupor@hpe.com>
2024-12-10 03:15:40 +0000

    Day3 part 2

--- 2024/3/solution/src/main.rs
+++ 2024/3/solution/src/main.rs
@@ -21,6 +21,31 @@ fn test_vec(s: &String) -> i64 {
     return result;
 }
 
+fn test_vec_part2(s: &String) -> i64 {
+    let mut result: i64 = 0;
+
+    // Find enabled ranges
+    let re = Regex::new(r"do\(\).*?().*?don't\(\)").unwrap();
+
+    // Find values in an enabled range
+    let re2 = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+
+    for mat in re.find_iter(s) {
+        let mul_args: Vec<(&str, &str)> = re2
+            .captures_iter(&s[mat.start()..mat.end()])
+            .map(|caps| {
+                let (_, [vala, valb]) = caps.extract();
+                (vala, valb)
+            })
+            .collect();
+        for captures in mul_args {
+            let (vala, valb) = captures;
+            result += i64::from_str(vala).unwrap() * i64::from_str(valb).unwrap();
+        }
+    }
+    return result;
+}
+
 fn parse(filename: &str) -> Vec<String> {
     let file_contents = fs::read_to_string(filename).unwrap();
     let lines = file_contents.split("\n");
@@ -35,14 +60,23 @@ fn main() -> io::Result<()> {
     let raw_strings: Vec<String> = parse("data");
     let mut total: i64 = 0;
 
+    let mut total_p2: i64 = 0;
+    let mut glob = String::new();
+
+    // Part 2: Mult is enabled at start of string.
+    glob += "do()";
     for string in raw_strings {
         total += test_vec(&string);
+        glob += &string; // Part 2: Glob everything into one long string.
     }
 
-    let mut total_p2: i64 = 0;
+    // Part 2: Regex cannot handle unterminated string. Terminate it with a don't().
+    glob += "don't()";
+
+    total_p2 += test_vec_part2(&glob);
 
     println!("Answer Part 1: {}", total);
-    println!("Answer Part 2: {}", total_p2 + total);
+    println!("Answer Part 2: {}", total_p2);
 
     Ok(())
 }
